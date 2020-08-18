6749f7316339d00debba8194029fa1dde6be3f7d
MikhailLysanov <69853717+MikhailLysanov@users.noreply.github.com>
2020-08-18 17:49:43 +0300

    Create create Main.java

---
+++ create Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "Ivan";
+        int age = 28;
+        String city = "from Moscow";
+        System.out.println("Hello "+name+"-"+age+"-"+city+"");
+
+
+    }
+}
