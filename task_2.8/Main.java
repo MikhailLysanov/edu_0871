6a88bd0f06a5969817dc38836accad2652baa355
MikhailLysanov <69853717+MikhailLysanov@users.noreply.github.com>
2020-08-25 16:59:36 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -1,1 +1,9 @@
-
+public class Main {
+    public static void main(String[] args) {
+        String str = "8";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(str);
+            str = str + "8";
+        }
+    }
+}
