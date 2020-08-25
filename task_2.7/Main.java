48f575641d7591540d00c0004f5c987d7d4db34f
MikhailLysanov <69853717+MikhailLysanov@users.noreply.github.com>
2020-08-25 16:58:49 +0300

    Update Main.java

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
+    }
 
+    public static int sumDigitsInNumber(int number) {
+        String str = String.valueOf(number);
+
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int x = Character.getNumericValue(c);
+            sum = sum + x;
+        }
+
+        return sum;
+    }
+}
