0bd0c9774330f6676e5c1ee487a395cd46adc87d
MikhailLysanov <69853717+MikhailLysanov@users.noreply.github.com>
2020-08-25 16:58:07 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[9];
 
+        for (int i = 0; i < 9; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 1; i <= 9; i++) {
+                int a = arr[j] * i;
+
+                System.out.print(a + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
