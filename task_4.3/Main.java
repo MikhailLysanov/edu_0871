932e2e17ae3b03fd7a6ebe29bd48891648e14b69
MikhailLysanov <69853717+MikhailLysanov@users.noreply.github.com>
2020-08-28 17:24:09 +0300

    Update Main.java

--- task_4.3/Main.java
+++ task_4.3/Main.java
@@ -1,1 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        Horse horse = new Horse(10, "Лошадь");
+        Pegasus pegasus = new Pegasus(50, "Пегас");
+        horse.run();
+        pegasus.fly();
+    }
+}
 
+abstract class Animal {
+    int age;
+    String name;
+
+    Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
+
+class Horse extends Animal {
+    Horse(int age, String name) {
+        super(age, name);
+    }
+
+    void run() {
+        System.out.println("Игого, я поскакал(а)");
+    }
+}
+
+class Pegasus extends Horse {
+    Pegasus(int age, String name) {
+        super(age, name);
+    }
+
+    void fly() {
+        System.out.println("Игого, я полетел(а)");
+    }
+}
